085739669852ad83cc640a2819c551938223468b
Minecube <126375609+Minecube1510@users.noreply.github.com>
2025-06-26 10:42:26 +0700

    Webbing

--- libs/script-js/page-core/favicon.js
+++ libs/script-js/page-core/favicon.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const favicon = (document.createElement('link'));
   favicon.rel = ('icon');
   favicon.type = ('image/png');
-  favicon.href = ('/libs/asset/favicon/emoji-test.ico');
+  favicon.href = ('../asset/favicon/emoji-test.ico');
 
   document.head.appendChild(favicon);
 });
