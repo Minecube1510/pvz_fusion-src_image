38bbf058322f0f02ad5ec651d6be5c8df1fe1458
Minecube <126375609+Minecube1510@users.noreply.github.com>
2025-06-26 10:20:17 +0700

    Webbing

--- libs/script-js/page-core/title.js
+++ libs/script-js/page-core/title.js
@@ -1,3 +1,11 @@
 /* title.js */
 
+document.addEventListener('DOMContentLoaded', () => {
+  const headTitleAttr = document.head.getAttribute('title');
+  if (headTitleAttr) {
+    document.title = `Halaman - ${headTitleAttr}`;
+  } else {
+    document.title = 'Halaman - Tanpa Judul';
+  }
+});
 
