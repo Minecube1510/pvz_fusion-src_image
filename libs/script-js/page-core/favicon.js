1c34160fc51401e44e962c83e2eb66ac0d8e8029
Minecube <126375609+Minecube1510@users.noreply.github.com>
2025-06-26 10:52:18 +0700

    .

--- libs/script-js/page-core/favicon.js
+++ libs/script-js/page-core/favicon.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const favicon = (document.createElement('link'));
   favicon.rel = ('icon');
   favicon.type = ('image/png');
-  favicon.href = ('../../asset/favicon/emoji-test.png');
+  favicon.href = ('./libs/asset/favicon/emoji-test.png');
   /* Intinya coba aja pake yang ICO dan PNG */
 
   document.head.appendChild(favicon);
